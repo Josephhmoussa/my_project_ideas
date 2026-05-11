d5f5469e49fbe7313c917b6105122494f33b3843
Joseph Moussa <josephmoussa@MacBookPro.home>
2026-05-11 19:04:28 +0200

    added normalization, enforce schema and S3 upload

--- assets/country_lifestyle_analytics/asset_weather_silver_ingestion.py
+++ assets/country_lifestyle_analytics/asset_weather_silver_ingestion.py
@@ -1,7 +1,9 @@
 import polars as pl
-import io
+import logging
 
 from utils.datalakeclient import S3Client
+
+logger = logging.getLogger(__name__)
 
 bucket_name = "country-lifestyle-analytics"
 datalake_client = S3Client(bucket_name=bucket_name)
@@ -9,11 +11,14 @@ datalake_client = S3Client(bucket_name=bucket_name)
 def ingest_weather_api_silver() -> pl.DataFrame:
     '''Ingest bronze data normalize and load to silver'''
 
+    logger.info("Starting Silver Layer ingestion")
+
     bronze_folder_path = f"bronze/weather"
 
-    # Get bronze files and load to df
+    # Get bronze files
     paths = datalake_client.get_paths_from_folder(bronze_folder_path)
 
+    # Load to Dataframes and concat
     dfs = []
     for path in paths:
         data = datalake_client.get_nested_json(path)
@@ -27,6 +32,55 @@ def ingest_weather_api_silver() -> pl.DataFrame:
 
         dfs.append(df)
 
-    return dfs
+    if not dfs:
+        logger.warning("No data found in bronze layer")
+        return pl.DataFrame()
+
+    df = pl.concat(dfs)
+
+    # Normalize columns
+    df = df.rename(
+        lambda col: (
+            col.strip()
+                .lower()
+                .replace(" ", "_")
+                .replace("-", "_")
+        )
+    )
+
+    # Enforce schema
+    df = df.with_columns([
+        pl.col("time")
+        .str
+        .to_date("%Y-%m-%d")
+        .alias("date"),
+
+        pl.col("temperature_2m_max")
+        .cast(pl.Float64),
+
+        pl.col("sunshine_duration")
+        .cast(pl.Float64),
+
+        pl.col("precipitation_sum")
+        .cast(pl.Float64),
+
+        pl.col("latitude")
+        .cast(pl.Float64),
+
+        pl.col("longitude")
+        .cast(pl.Float64)
+    ]).drop("time")
+
+    # Save to parquet and upload to S3
+    target_path = (
+        f"silver/"
+        f"weather/"
+        f"weather.parquet"
+    )
+
+    datalake_client.upload_dataframe_to_S3(target_path, df)
+    logger.info(f"Silver data uploaded to {target_path}")
+
+    return df
 
 print(ingest_weather_api_silver())
