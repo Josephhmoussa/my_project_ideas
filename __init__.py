fc5ef6e65d5656347af468e089fe3d231ff59762
Joseph Moussa <josephmoussa@MacBookPro.home>
2026-05-16 18:13:00 +0200

    added __init__.py + dagster Definitions

---
+++ __init__.py
@@ -0,0 +1,11 @@
+from dagster import Definitions, load_assets_from_package_module
+
+from . import assets
+
+# Load all assets
+dagster_assets = load_assets_from_package_module(assets)
+
+# Definitions
+defs = Definitions(
+    assets=dagster_assets
+)
