ed37aa30ccf0d8d077d7f5c4a4c929267473b395
Joseph Moussa <josephmoussa@MacBookPro.home>
2026-05-19 21:39:19 +0200

    refactored S3Client, now using AWS CLI

--- prod_pipeline/utils/datalakeclient.py
+++ prod_pipeline/utils/datalakeclient.py
@@ -1,24 +1,16 @@
 import boto3
 from botocore.config import Config
-import os
 import io
 import polars as pl
 import json
-from dotenv import load_dotenv
 
 
 class S3Client:
     def __init__(self, bucket_name: str):
         '''Initialize S3Client with credentials and bucket_name '''
 
-        load_dotenv()
-
         self.s3 = boto3.client(
             "s3",
-            aws_access_key_id = os.getenv("AWS_ACCESS_KEY_ID"),
-            aws_secret_access_key = os.getenv("AWS_SECRET_ACCESS_KEY"),
-            region_name = os.getenv("AWS_DEFAULT_REGION"),
-
             config = Config(
                 connect_timeout=60,
                 read_timeout=600
