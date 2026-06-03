c90395b5e8951596cbc93a1ddcfa16a42cad7a33
Joseph Moussa <josephmoussa@MacBookPro.home>
2026-06-03 18:29:24 +0200

    work in progress in asset ingest to bronze

--- prod_pipeline/assets/timesheet_tracker/assets_ingest_bronze_timesheet.py
+++ prod_pipeline/assets/timesheet_tracker/assets_ingest_bronze_timesheet.py
@@ -1,12 +1,31 @@
+from dagster import (
+    asset,
+    MaterializeResult,
+    MetadataValue,
+    AssetExecutionContext
+)
 import polars as pl
 from pathlib import Path
 from datetime import datetime, UTC
 
+from prod_pipeline.utils.datalakeclient import S3Client
+
 path = Path("prod_pipeline/assets/timesheet_tracker/csv_files/")
 
-def ingest_bronze_timesheet(path: Path) -> pl.DataFrame:
+@asset(
+        group_name="timesheet_pipeline",
+        compute_kind="python",
+        description="Ingest csv files locally and upload them to S3"
+    )
+def ingest_bronze_timesheet(context: AssetExecutionContext) -> MaterializeResult:
     '''Ingest csv files with minimal transformations and upload to S3'''
 
+    bucket_name = ""
+    s3_client = S3Client(bucket_name=bucket_name)
+
+    context.log.info("Extraction started")
+
+    # Loop through csv_files folder, extract data and concat to a single DataFrame
     dfs = []
     for file in path.rglob("*.csv"):
         category = str(file).split("/")[-1].split("_")[1]
@@ -24,8 +43,13 @@ def ingest_bronze_timesheet(path: Path) -> pl.DataFrame:
         ])
 
         dfs.append(df)
+    
+    df = pl.concat(dfs, how="diagonal_relaxed")
 
-    return dfs
+    # Upload to S3
+    target_path = ""
+
+    return df
 
 if __name__ == "__main__":
     result = ingest_bronze_timesheet(path)
