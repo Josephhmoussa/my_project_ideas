c3b3d8fa118de756e24cd883250352025e329186
Joseph Moussa <josephmoussa@MacBookPro.home>
2026-06-14 17:54:25 +0200

    added compliance cte to compliance layer

--- dbt/timesheet_tracker/models/mart/compliance.sql
+++ dbt/timesheet_tracker/models/mart/compliance.sql
@@ -22,7 +22,8 @@ ratios as (
         *,
         case when total_project_hours > 0 then (capex_hours / total_project_hours) else 0 end::float as capex_ratio,
         case when total_project_hours > 0 then (opex_hours / total_project_hours) else 0 end::float as opex_ratio,
-        case when leave_hours in (37.5, 40) then 1 else 0 end as is_leave_week
+        case when leave_hours in (37.5, 40) then 1 else 0 end as is_leave_week,
+        case when created_not_submitted_hours != 0 or not_submitted_hours != 0 then 1 else 0 end as has_non_submitted
     from aggregated_metrics
 ),
 
@@ -30,16 +31,27 @@ conditions as (
     select
         *,
         case
-            when total_hours between 35 and 41 and not_submitted_hours = 0 then 1 else 0 end as time_ok,
+            when total_hours between 35 and 41 and has_non_submitted = 0 then 1 else 0 end as time_ok,
         case    
             when is_leave_week = 1 then 1
             when capex_ratio between 0.10 and 0.30 and opex_ratio between 0.70 and 0.90 then 1
             else 0
         end as mix_ok,
         case
-            when admin_hours <= 20 and not_submitted_hours = 0 then 1 else 0 end as admin_ok
+            when admin_hours <= 20 and has_non_submitted = 0 then 1 else 0 end as admin_ok
     from ratios
+),
+
+compliance as (
+    select
+        *,
+        case
+            when has_non_submitted = 0 and mix_ok = 1 and time_ok = 1 and admin_ok = 1 then 2
+            when has_non_submitted = 0 and not (mix_ok = 1 and time_ok = 1 and admin_ok = 1) then 1
+            else 0
+        end as is_compliant
+    from conditions
 )
 
 select *
-from conditions
+from compliance
